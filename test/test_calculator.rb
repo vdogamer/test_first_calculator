12934031520a2424e63e6e85c7a840240b1bc24d
vdogamer <ctd8z@virginia.edu>
2015-01-07 16:05:00 +0000

    Adding subtract test

--- test/test_calculator.rb
+++ test/test_calculator.rb
@@ -7,5 +7,11 @@ class CalculatorTest < Minitest::Test #Test::Unit::TestCase
     calculator = Calculator.new
     actual = calculator.add(2, 2)
     assert_equal(4, actual)
-  end  
+  end 
+  
+  def test_can_subtract
+    calculator = Calculator.new
+    actual = calculator.subtract(5,3)
+    assert_equal(2, actual)
+  end
 end
